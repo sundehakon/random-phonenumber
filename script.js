04951067f8ddead1f3cb530f2f2075bc40ac8f15
sundehakon <142574795+sundehakon@users.noreply.github.com>
2023-10-11 19:45:59 +0200

    fixed prompt issue

---
+++ script.js
@@ -0,0 +1,22 @@
+const prompt = require('prompt-sync')();
+
+const result = prompt("Do you wish for a random telephone number? (y/n)");
+
+const phoneNumber = [];
+for (i = 0; i < 8; i += 1) {
+    phoneNumber[i] = Math.round(Math.random() * 9)
+}
+
+if (result.toString().startsWith('y')) {
+    console.log(("Awesome, heres a random telephone number! :"),phoneNumber.join(""))
+} else if (result.toString().startsWith('n')) {
+    console.log(('Goodbye.'))
+} else if (result !== 'y') {
+    console.log("Please enter either y or n")
+} else if (result !== 'n') {
+    console.log("Please enter either y or n")
+}
+
+for (i = 0; i < 8; i += 1) {
+    phoneNumber[i] = Math.round(Math.random() * 9)
+}
