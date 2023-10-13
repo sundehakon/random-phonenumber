1ec1279203bc85e60ced0b25923e10b5ff66efbb
sundehakon <142574795+sundehakon@users.noreply.github.com>
2023-10-13 09:22:38 +0200

    coded arecode for loop, and added ps message

--- script.js
+++ script.js
@@ -12,15 +12,20 @@ if (result.toString().startsWith('n')) {
     phoneNumber.splice(6, 0, ' ')
 
     console.log(("Here's a random Norwegian telephone number! :"),(`+47 ${phoneNumber.join("")}`))
-} else if (result.toString().startsWith('u')) {
+} else if (result.toString().startsWith('a')) {
     const usPhoneNumber = [];
     const areaCode = [];
+
+    for (i = 0; i < 3; i += 1) {
+        areaCode[i] = Math.round(Math.random() * 9)
+    }
 
     for (i = 0; i < 7; i += 1) {
         usPhoneNumber[i] = Math.round(Math.random() * 9)
     }
     
-    console.log(("Here's a random American telephone number!"), (`(${areaCode}) ${usPhoneNumber.slice(0, 3).join("")}-${usPhoneNumber.slice(3).join("")}`))
+    console.log(("Here's a random American telephone number!"), (`(${areaCode.join("")}) ${usPhoneNumber.slice(0, 3).join("")}-${usPhoneNumber.slice(3).join("")}`))
+    console.log("PS! Area code might be wrong, we're working on a solution.")
 } else if (result !== 'a') {
     console.log("Please enter either a or n")
 } else if (result !== 'n') {
