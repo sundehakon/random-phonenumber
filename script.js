9b8589333f3ec8189545cbc87da6ca4517cd7ecd
sundehakon <142574795+sundehakon@users.noreply.github.com>
2023-10-13 08:53:10 +0200

    started inserting the american version into code

--- script.js
+++ script.js
@@ -1,25 +1,21 @@
 const prompt = require('prompt-sync')();
 
-const result = prompt("Do you wish for a random telephone number? (y/n)");
+const result = prompt("Do you want a Norwegian, or American phone number? (u/n)");
 
-const phoneNumber = [];
-for (i = 0; i < 8; i += 1) {
-    phoneNumber[i] = Math.round(Math.random() * 9)
-}
+if (result.toString().startsWith('n')) {
+    const phoneNumber = [];
+    for (i = 0; i < 8; i += 1) {
+        phoneNumber[i] = Math.round(Math.random() * 9)
+    }
+    
+    phoneNumber.splice(3, 0, ' ')
+    phoneNumber.splice(6, 0, ' ')
 
-phoneNumber.splice(3, 0, ' ')
-phoneNumber.splice(6, 0, ' ')
-
-if (result.toString().startsWith('y')) {
-    console.log(("Awesome, here's a random telephone number! :"),(`+47 ${phoneNumber.join("")}`))
-} else if (result.toString().startsWith('n')) {
-    console.log(('Goodbye.'))
+    console.log(("Awesome, here's a random Norwegian telephone number! :"),(`+47 ${phoneNumber.join("")}`))
+} else if (result.toString().startsWith('u')) {
+    console.log(("Awesome here's a random American telephone number!"))
 } else if (result !== 'y') {
-    console.log("Please enter either y or n")
+    console.log("Please enter either u or n")
 } else if (result !== 'n') {
-    console.log("Please enter either y or n")
-}
-
-for (i = 0; i < 8; i += 1) {
-    phoneNumber[i] = Math.round(Math.random() * 9)
+    console.log("Please enter either u or n")
 }
